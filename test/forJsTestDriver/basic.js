a16d2f3b9167df2fec7e956991cd4060986b9df4
Phillip Heidegger <git@heidegger.eu>
2010-10-15 10:22:29 +0200

    uses data handler for easier mapping between expected check result and realized check result.

--- test/forJsTestDriver/basic.js
+++ test/forJsTestDriver/basic.js
@@ -12,7 +12,36 @@
 		var tc = TestCase(m + " - " + i);
 		tc.prototype["test" + i] = f;				
 	}
-
+	function makeChecker(exp) {
+		return {"__default__": 
+  		function (msg) {
+  			if (exp !== msg) {
+					switch (msg) {
+					case "fail": 
+						fail( "Expected: " + exp 
+					    			+ ", got message: " + msg 
+									+ ", value: " + arguments[1]
+									+ ", constraint: " + arguments[2]);
+						break;
+					case "success":
+					  	fail( "Expected: " + exp 
+					    			+ ", got message: " + msg 
+									+ ", value: " + arguments[1]
+									+ ", constraint: " + arguments[2]);
+						break;
+					case "error":
+					  	fail( "Expected: " + exp 
+					    			+ ", got message: " + msg 
+									+ ", error message: " + arguments[1]
+									+ ", constraint: " + arguments[2]);
+						break;
+					}
+				}
+  	  }
+  	};
+	}
+	
+	
 	var anz = 1;
 	
 	var u = JSConTest.contracts.Name("u");
@@ -40,243 +69,79 @@
 		x
 	];
 	var vals = [
-		{v: null, d: "Null"},
-		{v: undefined, d: "Udf"},
-		{v: true, d: "True"},
-		{v: false, d: "False"},
-		{v: 0, d: "Integer"},
-		{v: Number.NaN, d: "NaN" },
-		{v: 1.5, d: "Float"},
-		{v: "str", d: "String"},
-		{v: {}, d: "Obj" },
-		{v: [1,2], d: "Array[int]" },
-		{v: [true,false,true], d: "Array[boolean]" },
+		{ v: null, 
+			d: "Null", 
+		  expected: ['success', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail',
+			           'fail', 'fail', 'fail', 'fail', 'fail', 'success', 'fail', 'fail',
+			           'error', 'fail']
+		},
+		{ v: undefined, 
+			d: "Udf",
+		  expected: ['fail', 'success', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail',
+		             'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail',
+		             'error', 'success']
+		},
+		{ v: true, 
+			d: "True",
+			expected: ['fail', 'fail', 'success', 'fail', 'success', 'fail', 'fail', 'fail', 'fail', 
+			           'fail', 'fail', 'fail', 'fail', 'fail', 'success', 'error', 'fail']
+		},
+		{ v: false, 
+			d: "False",
+			expected: ['fail', 'fail', 'fail', 'success', 'success', 'fail', 'fail', 'fail', 'fail',
+			           'fail', 'fail', 'fail', 'fail', 'fail', 'success', 'error', 'fail']
+		},
+		{ v: 0, 
+			d: "Integer",
+			expected: ['fail', 'fail', 'fail', 'fail', 'fail', 'success', 'success', 'success', 
+			           'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'error', 'fail'
+			           ]				
+		},
+		{ v: Number.NaN, 
+			d: "NaN", 
+			expected: ['fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 
+			           'fail', 'fail', 'fail', 'fail', 'fail', 'error', 'fail']
+		},
+		{ v: 1.5, 
+			d: "Float",
+			expected: ['fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'success',
+			           'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'error', 'fail']
+		},
+		{ v: "str", 
+			d: "String",
+			expected: ['fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'success', 
+			           'fail', 'fail', 'fail', 'fail', 'success', 'fail', 'error', 'success']
+		},
+		{ v: {}, 
+			d: "Obj",
+			expected: ['fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 
+			           'success', 'fail', 'fail', 'success', 'success', 'fail', 'error', 'fail']			
+		},
+		{ v: [1,2], 
+			d: "Array[int]",
+			expected: ['fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 
+			           'success', 'fail', 'success', 'success', 'success', 'fail', 'error', 'fail']
+		},
+		{ v: [true,false,true], 
+			d: "Array[boolean]", 
+			expected: ['fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 'fail', 
+				         'success', 'success', 'success', 'success', 'success', 'fail', 'error', 'fail']
+		},
 	];
-	for (var j in vals) {
-		var v = vals[j].v;
-		var d = vals[j].d;
-		for (var i in contr) {
-			JSConTest.tests.add(d, v, contr[i],anz);
-			// success, fail, error
+	
+	var j, i, v,d,exp;
+	for (j in vals) {
+		v = vals[j].v;
+		d = vals[j].d;
+		vals[j].expected.reverse();
+		for (i in contr) {
+			exp = vals[j].expected.pop();
+			(function (v, d, c, exp) {
+				JSConTest.tests.add(d, v, c, anz, {checker: makeChecker(exp)});
+			}(v, d, contr[i], exp));
 		}
 	}
 	  
-//	var dl = JSConTest.events.enum.create('logger',"ul");
-//	JSConTest.events.register(dl);
-	  
-	  
-	var ch = checker(['statistic', 'cancel','CExpStart'],[
-	    // Null
-		{ msg: 'moduleChange' },
-		{ msg: 'fail' },
-		{ msg: 'error' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		 // Udf
-		{ msg: 'moduleChange' },
-		{ msg: 'success' },
-		{ msg: 'error' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		 // True
-		{ msg: 'moduleChange' },
-		{ msg: 'fail' },
-		{ msg: 'error' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		// False
-		{ msg: 'moduleChange' },
-		{ msg: 'fail' },
-		{ msg: 'error' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		// Integer
-		{ msg: 'moduleChange' },
-		{ msg: 'fail' },
-		{ msg: 'error' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'success' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		// NaN
-		{ msg: 'moduleChange' },
-		{ msg: 'fail' },
-		{ msg: 'error' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		// Float
-		{ msg: 'moduleChange' },
-		{ msg: 'fail' },
-		{ msg: 'error' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		// String
-		{ msg: 'moduleChange' },
-		{ msg: 'success' },
-		{ msg: 'error' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		// Object
-		{ msg: 'moduleChange' },
-		{ msg: 'fail' },
-		{ msg: 'error' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		// Array[int]
-		{ msg: 'moduleChange' },
-		{ msg: 'fail' },
-		{ msg: 'error' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'success' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		// Array[boolean]
-		{ msg: 'moduleChange' },
-		{ msg: 'fail' },
-		{ msg: 'error' },
-		{ msg: 'fail' },
-		{ msg: 'success' },
-		{ msg: 'success' },
-		{ msg: 'success' },
-		{ msg: 'success' },
-		{ msg: 'success' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-		{ msg: 'fail' },
-	]);
-	JSConTest.events.register(ch);
-
+	JSConTest.events.register(JSConTest.events.handler.data.create("checker"));  
 	JSConTest.tests.runLazy(registerForJSTestDriver);
 }());
