5b20ffa45246fc330da3e848b04b955f68953cff
Annette Bieniusa <bieniusa@informatik.uni-freiburg.de>
2010-10-23 21:36:56 +0200

    test case with effect that allows anything, leads to a too-much-recursion error in firefox

--- test/forJsTestDriver/linked-list-test.js
+++ test/forJsTestDriver/linked-list-test.js
@@ -1,8 +1,11 @@
 var LinkedList = (function  () {
       function LinkedList_own ()  {
             JSConTest.tests.setVar("c_2", JSConTest.tests.assertParams(["c_1"], arguments, LinkedList_own, "LinkedList"));
-            JSConTest.effects.propAss(this, "_length", 0. );
-            JSConTest.effects.propAss(this, "_head", null);
+            if (JSConTest.effects.isBox(this)) {
+                JSConTest.effects.propAss(this, "_length", 0. );
+                JSConTest.effects.propAss(this, "_head", null);
+              }
+              else return JSConTest.tests.getVar("c_2").assertReturn(LinkedList_own.apply(JSConTest.effects.box_this(this), arguments));;
           };
         (function  () {
           function LinkedList () /* with lv: */ {
@@ -13,62 +16,54 @@ var LinkedList = (function  () {
         })();
         return LinkedList_own;
     })();
-  var remove = (function  () {
-      function remove_own (index)  {
-            JSConTest.tests.setVar("c_5", JSConTest.tests.assertParams(["c_4"], arguments, remove_own, "remove"));
-            var index = arguments[0. ];
-            if (((JSConTest.effects.unbox(index) > 1. ) && (JSConTest.effects.unbox(index) < JSConTest.effects.unbox(JSConTest.effects.propAcc(this, "_length"))))) {
-                var current = JSConTest.effects.box("current", JSConTest.effects.propAcc(this, "_head")),
-                  previous,
-                  i = 0. ;
-                if ((JSConTest.effects.unbox(index) === 0. )) {
-                  JSConTest.effects.propAss(this, "_head", JSConTest.effects.propAcc(current, "next"));
+  var add = (function  () {
+      function add_own (data)  {
+            JSConTest.tests.setVar("c_5", JSConTest.tests.assertParams(["c_4"], arguments, add_own, "add"));
+            if (JSConTest.effects.isBox(this)) {
+                var data = JSConTest.effects.box_param(1. , arguments[0. ]);
+                var node = JSConTest.effects.box("node", {data : JSConTest.effects.unbox(data), next : null}),
+                  current;
+                if ((JSConTest.effects.unbox(JSConTest.effects.propAcc(this, "_head")) === null)) {
+                  JSConTest.effects.propAss(this, "_head", node);
                 }
                 else {
-                  while ((JSConTest.effects.unbox(index) > JSConTest.effects.unbox(i++ ))) {
-                    previous = current;
+                  current = JSConTest.effects.propAcc(this, "_head");
+                  while (JSConTest.effects.propAcc(current, "next")) {
                     current = JSConTest.effects.propAcc(current, "next");
                   }
-                  JSConTest.effects.propAss(previous, "next", JSConTest.effects.propAcc(current, "next"));
+                  JSConTest.effects.propAss(current, "next", node);
                 };
-                JSConTest.effects.unOp("-- ", this, "_length");
-                return JSConTest.tests.getVar("c_5").assertReturn(JSConTest.effects.propAcc(current, "data"));
+                JSConTest.effects.unOp("++ ", this, "_length");
               }
-              else {
-                return JSConTest.tests.getVar("c_5").assertReturn(null);
-              };
+              else return JSConTest.tests.getVar("c_5").assertReturn(add_own.apply(JSConTest.effects.box_this(this), arguments));;
           };
         (function  () {
-          function remove (index) /* with lv: current,previous,i*/ {
-                if (((index >  -1. ) && (index < this._length))) {
-                    var current = this._head,
-                      previous,
-                      i = 0. ;
-                    if ((index === 0. )) {
-                      this._head = current.next;
-                    }
-                    else {
-                      while ((index > i++ )) {
-                        previous = current;
-                        current = current.next;
-                      }
-                      previous.next = current.next;
-                    };
-                    this._length-- ;
-                    return current.data;
+          function add (data) /* with lv: node,current*/ {
+                var node = {data : data, next : null},
+                    current;
+                if ((this._head === null)) {
+                    this._head = node;
                   }
                   else {
-                    return null;
+                    current = this._head;
+                    while (current.next) {
+                      current = current.next;
+                    }
+                    current.next = node;
                   };
+                this._length++ ;
               };
-            JSConTest.tests.overrideToStringOfFunction(remove_own, remove);
+            JSConTest.tests.overrideToStringOfFunction(add_own, add);
         })();
-        return remove_own;
+        return add_own;
     })();
   var size = (function  () {
       function size_own ()  {
             JSConTest.tests.setVar("c_8", JSConTest.tests.assertParams(["c_7"], arguments, size_own, "size"));
-            return JSConTest.tests.getVar("c_8").assertReturn(JSConTest.effects.propAcc(this, "_length"));
+            if (JSConTest.effects.isBox(this)) {
+                return JSConTest.tests.getVar("c_8").assertReturn(JSConTest.effects.unbox(JSConTest.effects.propAcc(this, "_length")));
+              }
+              else return JSConTest.tests.getVar("c_8").assertReturn(size_own.apply(JSConTest.effects.box_this(this), arguments));;
           };
         (function  () {
           function size () /* with lv: */ {
@@ -81,13 +76,16 @@ var LinkedList = (function  () {
   var toArray = (function  () {
       function toArray_own ()  {
             JSConTest.tests.setVar("c_11", JSConTest.tests.assertParams(["c_10"], arguments, toArray_own, "toArray"));
-            var result = JSConTest.effects.box("result", []),
-                current = JSConTest.effects.box("current", JSConTest.effects.propAcc(this, "_head"));
-            while (current) {
-                JSConTest.effects.mCall(result, "push", [JSConTest.effects.propAcc(current, "data")]);
-                current = JSConTest.effects.propAcc(current, "next");
+            if (JSConTest.effects.isBox(this)) {
+                var result = JSConTest.effects.box("result", []),
+                  current = JSConTest.effects.box("current", JSConTest.effects.propAcc(this, "_head"));
+                while (current) {
+                  JSConTest.effects.mCall(result, "push", [JSConTest.effects.propAcc(current, "data")]);
+                  current = JSConTest.effects.propAcc(current, "next");
+                }
+                return JSConTest.tests.getVar("c_11").assertReturn(JSConTest.effects.unbox(result));
               }
-            return JSConTest.tests.getVar("c_11").assertReturn(result);
+              else return JSConTest.tests.getVar("c_11").assertReturn(toArray_own.apply(JSConTest.effects.box_this(this), arguments));;
           };
         (function  () {
           function toArray () /* with lv: result,current*/ {
@@ -106,7 +104,10 @@ var LinkedList = (function  () {
   var toString = (function  () {
       function toString_own ()  {
             JSConTest.tests.setVar("c_15", JSConTest.tests.assertParams(["c_14"], arguments, toString_own, "toString"));
-            return JSConTest.tests.getVar("c_15").assertReturn(JSConTest.effects.mCall(JSConTest.effects.mCall(this, "toArray", []), "toString", []));
+            if (JSConTest.effects.isBox(this)) {
+                return JSConTest.tests.getVar("c_15").assertReturn(JSConTest.effects.unbox(JSConTest.effects.mCall(JSConTest.effects.mCall(this, "toArray", []), "toString", [])));
+              }
+              else return JSConTest.tests.getVar("c_15").assertReturn(toString_own.apply(JSConTest.effects.box_this(this), arguments));;
           };
         (function  () {
           function toString () /* with lv: */ {
@@ -117,29 +118,29 @@ var LinkedList = (function  () {
         return toString_own;
     })();
   (function  () {
-    var ga092b4d174279154970ebda754f729160 = JSConTest.contracts.Function([], JSConTest.contracts.Top, [], "LinkedList");
-      JSConTest.tests.add("LinkedList", LinkedList, ga092b4d174279154970ebda754f729160, 1000. );
-      JSConTest.tests.setVar("c_1", ga092b4d174279154970ebda754f729160);
+    var gcd3a176cfa0b5f3a5e6e4df18d73ba790 = JSConTest.contracts.Function([], JSConTest.contracts.Top, [], "LinkedList");
+      JSConTest.tests.add("LinkedList", LinkedList, gcd3a176cfa0b5f3a5e6e4df18d73ba790, 1000. );
+      JSConTest.tests.setVar("c_1", gcd3a176cfa0b5f3a5e6e4df18d73ba790);
   })();
   (function  () {
-    var ga092b4d174279154970ebda754f729163 = JSConTest.contracts.Method(JSConTest.contracts.EObject([{name : "_length", contract : JSConTest.contracts.Integer}, {name : "_head", contract : JSConTest.contracts.EObject([])}]), [JSConTest.contracts.Integer], JSConTest.contracts.Top, [], "remove");
-      JSConTest.tests.add("remove", remove, ga092b4d174279154970ebda754f729163, 1000. );
-      JSConTest.tests.setVar("c_4", ga092b4d174279154970ebda754f729163);
+    var gcd3a176cfa0b5f3a5e6e4df18d73ba793 = JSConTest.contracts.Method(JSConTest.contracts.EObject([{name : "_length", contract : JSConTest.contracts.Integer}, {name : "_head", contract : JSConTest.contracts.EObject([])}]), [JSConTest.contracts.Integer], JSConTest.contracts.Undefined, [], "add");
+      JSConTest.tests.add("add", add, gcd3a176cfa0b5f3a5e6e4df18d73ba793, 1000. );
+      JSConTest.tests.setVar("c_4", gcd3a176cfa0b5f3a5e6e4df18d73ba793);
   })();
   (function  () {
-    var ga092b4d174279154970ebda754f729166 = JSConTest.contracts.Method(JSConTest.contracts.EObject([{name : "_length", contract : JSConTest.contracts.Integer}]), [], JSConTest.contracts.Integer, [{type : 7. , effect : {type : 3. , property : "_length", effect : {type : 8. , fname : "size"}}}], "size");
-      JSConTest.tests.add("size", size, ga092b4d174279154970ebda754f729166, 1000. );
-      JSConTest.tests.setVar("c_7", ga092b4d174279154970ebda754f729166);
+    var gcd3a176cfa0b5f3a5e6e4df18d73ba796 = JSConTest.contracts.Method(JSConTest.contracts.EObject([{name : "_length", contract : JSConTest.contracts.Integer}]), [], JSConTest.contracts.Integer, [], "size");
+      JSConTest.tests.add("size", size, gcd3a176cfa0b5f3a5e6e4df18d73ba796, 1000. );
+      JSConTest.tests.setVar("c_7", gcd3a176cfa0b5f3a5e6e4df18d73ba796);
   })();
   (function  () {
-    var ga092b4d174279154970ebda754f729169 = JSConTest.contracts.Method(JSConTest.contracts.EObject([{name : "_head", contract : JSConTest.contracts.EObject([])}]), [], JSConTest.contracts.Array(JSConTest.contracts.Top), [{type : 7. , effect : {type : 5. , effect : {type : 8. , fname : "toArray"}}}], "toArray");
-      JSConTest.tests.add("toArray", toArray, ga092b4d174279154970ebda754f729169, 1000. );
-      JSConTest.tests.setVar("c_10", ga092b4d174279154970ebda754f729169);
+    var gcd3a176cfa0b5f3a5e6e4df18d73ba799 = JSConTest.contracts.Method(JSConTest.contracts.EObject([{name : "_head", contract : JSConTest.contracts.EObject([])}]), [], JSConTest.contracts.Array(JSConTest.contracts.Top), [{type : 7. , effect : {type : 5. , effect : {type : 2. , name : "this", fname : "toArray"}}}], "toArray");
+      JSConTest.tests.add("toArray", toArray, gcd3a176cfa0b5f3a5e6e4df18d73ba799, 1000. );
+      JSConTest.tests.setVar("c_10", gcd3a176cfa0b5f3a5e6e4df18d73ba799);
   })();
   (function  () {
-    var ga092b4d174279154970ebda754f7291612 = JSConTest.contracts.Method(JSConTest.contracts.EObject([{name : "_head", contract : JSConTest.contracts.EObject([])}]), [], JSConTest.contracts.Array(JSConTest.contracts.Top), [], "toString");
-      var ga092b4d174279154970ebda754f7291613 = JSConTest.contracts.Method(JSConTest.contracts.EObject([{name : "toArray", contract : ga092b4d174279154970ebda754f7291612}]), [], JSConTest.contracts.String, [{type : 7. , effect : {type : 5. , effect : {type : 8. , fname : "toString"}}}], "toString");
-      JSConTest.tests.add("toString", toString, ga092b4d174279154970ebda754f7291613, 1000. );
-      JSConTest.tests.setVar("c_14", ga092b4d174279154970ebda754f7291613);
+    var gcd3a176cfa0b5f3a5e6e4df18d73ba7912 = JSConTest.contracts.Method(JSConTest.contracts.EObject([{name : "_head", contract : JSConTest.contracts.EObject([])}]), [], JSConTest.contracts.Array(JSConTest.contracts.Top), [], "toString");
+      var gcd3a176cfa0b5f3a5e6e4df18d73ba7913 = JSConTest.contracts.Method(JSConTest.contracts.EObject([{name : "toArray", contract : gcd3a176cfa0b5f3a5e6e4df18d73ba7912}]), [], JSConTest.contracts.String, [], "toString");
+      JSConTest.tests.add("toString", toString, gcd3a176cfa0b5f3a5e6e4df18d73ba7913, 1000. );
+      JSConTest.tests.setVar("c_14", gcd3a176cfa0b5f3a5e6e4df18d73ba7913);
   })();
-  LinkedList.prototype = {constructor : LinkedList, size : size, toArray : toArray, toString : toString};
+  LinkedList.prototype = {constructor : LinkedList, add : add, size : size, toArray : toArray, toString : toString};
