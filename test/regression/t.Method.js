df0488bf3d8c32ecf19a56df9f446f299e52a205
Phillip Heidegger <git@heidegger.eu>
2011-07-06 18:10:18 +0200

    Simple Method Test

---
+++ test/regression/t.Method.js
@@ -0,0 +1,13 @@
+/*c () -> undefined */
+function f() {
+    var o = { x : 5 };
+    o.m = m;
+    var x = o.m(2);
+    if (x !== 5) {
+	throw "Should not happen, x is unequal to 5";
+    };
+}
+
+function m(x) {
+    return this.x;
+}
